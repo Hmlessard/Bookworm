37a18f3060ea393889f75200198d62ab5a6b1283
Tony Linz <tonyllinz@gmail.com>
2022-03-08 17:19:54 -0600

    get all posts, users, one user by id, post create a user, login/out, put update username or pass, and delete a user routes work.

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,155 @@
+const router = require('express').Router();
+const { User, Post, Comment, Book } = require('../../models');
+
+//get /api/users
+router.get('/', (req, res) =>{
+    //access our User model and run .findAll() method
+    User.findAll({
+        //attributes: {exclude: ['password']}
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//get /api/users/1
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        //attributes: {exclude: ['password']},
+        include: [
+            {
+                model: Post,
+                //attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                }
+            }
+        ]
+
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//post /api/users
+router.post('/', (req, res) => {
+    //body expects {username, password}
+    User.create(req.body)
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/login', (req, res) =>{
+    //expects {username: 'TonyL', password: 'password'}
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({message: 'No user with that username!'});
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({message: 'Incorrect password!'});
+            return;
+        }
+        
+        req.session.save(() => {
+            //declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({user: dbUserData, message: 'You are now logged in!'});
+        })
+    })
+})
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+})
+
+//put /api/users/1
+router.put('/:id', (req, res) => {
+    //expects {username, password}
+
+    //if req.body has exact key/value pairs to match the model, you can just use 'req.body' instead
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then (dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//delete /api/users/1
+router.delete('/:id', (req,res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const path = require('path');
+//const path = require('path');
 // const exphbs = require('express-handlebars');
 const session = require('express-session');
 // const helpers = require('./utils/helpers');
@@ -31,7 +31,7 @@ app.use(session(sess));
 // app.set('view engine', 'handlebars');
 
 //turn on routes
-// app.use(routes);
+app.use(routes);
 
 //turn on connection to db and server
 sequelize.sync({force: false}).then(() => {
