b0151dd1419af049900c19dc01375adef997c769
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-03-08 20:45:27 -0600

    post, comment, book api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-//const postRoutes = require('./post-routes.js');
-//const commentRoutes = require('./comment-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
+const bookRoutes = require('./book-routes');
 
 router.use('/users', userRoutes);
-//router.use('/posts', postRoutes);
-//router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+router.use('/books', bookRoutes);
 
 module.exports = router;
