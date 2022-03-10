3b4b7f57c50af6b9cefedfae6c5004e4f4f22a47
Tony Linz <tonyllinz@gmail.com>
2022-03-09 21:14:28 -0600

    pulled comment partial, add comment on single post works, just need to render comments and single-post is donion

---
+++ public/javascript/comment.js
@@ -0,0 +1,32 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+    console.log('subd');
+  
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const user_id = 3;
+  
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          comment_text,
+          post_id,
+          user_id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      if (response.ok) {
+        document.location.reload();
+      } else {
+            alert(response.statusText);
+        }
+    };
+};
+  
+  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
