f5a8c807b508c313964ed97c453b2bc99d1acbc2
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-03-09 20:37:02 -0600

    added homepage route to home-routes.js and homepage handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,17 +2,42 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Post, Comment, Book } = require('../models');
 
-router.get('/', (req, res) => {    
+// view home page with hero image and all the posts
+router.get('/', (req, res) => {
     Post.findAll({
-
+        attributes: [
+            'id',
+            'book_title',
+            'book_author',
+            'book_review',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
     })
-    .then(dbUserData => {
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn // only users logged in can see the posts? 
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
 });
 
 // router.get('/login', (req,res) => {
@@ -20,7 +45,7 @@ router.get('/', (req, res) => {
 //         res.redirect('/');
 //         return;
 //     }
-    
+
 //     res.render('login');
 // });
 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-//const path = require('path');
-// const exphbs = require('express-handlebars');
+const path = require('path');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
 // const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({});
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -23,17 +23,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //turn on routes
 app.use(routes);
 
 //turn on connection to db and server
-sequelize.sync({force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
 });
