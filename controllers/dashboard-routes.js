3cd8bef3425442f557566894f9bbc988387049d1
Lindsey Dubnicka <lindseymiller2567@gmail.com>
2022-03-11 17:58:48 -0600

    started on dashboard handlebar page and routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,47 +1,42 @@
-// const router = require('express').Router();
-// const sequelize = require('../config/connection');
-// const { Post, User, Comment, Vote } = require('../models');
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Book } = require('../models');
 // const withAuth = require('../utils/auth');
 
-// // get all posts for dashboard
-// router.get('/', withAuth, (req, res) => {
-//   console.log(req.session);
-//   console.log('======================');
-//   Post.findAll({
-//     where: {
-//       user_id: req.session.user_id
-//     },
-//     attributes: [
-//       'id',
-//       'post_url',
-//       'title',
-//       'created_at',
-//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//     ],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//         include: {
-//           model: User,
-//           attributes: ['username']
-//         }
-//       },
-//       {
-//         model: User,
-//         attributes: ['username']
-//       }
-//     ]
-//   })
-//     .then(dbPostData => {
-//       const posts = dbPostData.map(post => post.get({ plain: true }));
-//       res.render('dashboard', { posts, loggedIn: true });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+// get all User's posts for dashboard
+router.get('/dashboard', (req, res) => {
+    console.log(req.session);
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'book_title',
+            'book_author',
+            'book_review',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // router.get('/edit/:id', withAuth, (req, res) => {
 //   Post.findByPk(req.params.id, {
@@ -70,7 +65,7 @@
 //     .then(dbPostData => {
 //       if (dbPostData) {
 //         const post = dbPostData.get({ plain: true });
-        
+
 //         res.render('edit-post', {
 //           post,
 //           loggedIn: true
@@ -84,4 +79,4 @@
 //     });
 // });
 
-// module.exports = router;
+module.exports = router;
